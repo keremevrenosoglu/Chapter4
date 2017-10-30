9ef15aaaa68076e38cfe04acc9670a775648e267
keremevrenosoglu <31323087+keremevrenosoglu@users.noreply.github.com>
2017-10-30 10:58:44 -0700

    Chapter4

---
+++ ChapterFour/Student.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    // instance variables - replace the example below with your own
+    private int idNumber;
+    private String name;
+    private Student bestFriend;
+    public Student() {
+        
+}
+public Student(int num, String nm) {
+    this.idNumber = num;
+    this.name = nm;
+
+
+}
+public String getName() {
+    return this.name;
+   
+}
+public void combineIDs() {
+    return this.idNumber + bestFriend.idNumber;
+}
+public Student makeStudent() {
+    Student joe = new Student(this.idNumber, bestFriend.name);
+    return joe;
+}
+public String toString() {
+    return "THe student name is: " + this.name;
+}
+}
---
+++ ChapterFour/StudentDriver.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class StudentDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudentDriver
+{
+    public static void main() {
+     Student sam = new Student(1234, "Sam");  
+     
+     
+     System.out.println(sam);
+}
+}
